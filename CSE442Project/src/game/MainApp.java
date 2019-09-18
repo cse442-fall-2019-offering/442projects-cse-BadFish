93f2e0957547a1da2cf041349624589731790dbe
mohanma <mohanma@mohans-air.fios-router.home>
2019-09-18 00:53:42 -0400

    I create menu panel and user profile panel, both of them under the
    view package. The root layout is use for file loading, editing or
    
    helping options. You can running the MainApp.java to see the menu
    
    panel and by deselecting showUserProfilePanel() to check the user
    
    profile panel.

---
+++ CSE442Project/src/game/MainApp.java
@@ -0,0 +1,93 @@
+package game;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Coding Game");
+
+        initRootLayout();
+
+        showMenuPanel();
+                
+        //showUserProfilePanel();
+    }
+    
+    /**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+            
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Shows the menu panel inside the root layout.
+     */
+    public void showMenuPanel() {
+        try {
+            // Load menu panel.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/MenuPanel.fxml"));
+            AnchorPane menuPanel = (AnchorPane) loader.load();
+            
+            // Set menu panel into the center of root layout.
+            rootLayout.setCenter(menuPanel);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * Shows the user profile panel inside the root layout.
+     */
+    public void showUserProfilePanel() {
+        try {
+            // Load user profile panel.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/UserProfilePanel.fxml"));
+            AnchorPane menuPanel = (AnchorPane) loader.load();
+            
+            // Set user profile panel into the center of root layout.
+            rootLayout.setCenter(menuPanel);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * Returns the main stage.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
