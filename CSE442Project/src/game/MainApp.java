7eadea44dccc64a2e2584908003a1f41af2a42e3
tebas <tebas@DESKTOP-JA818O1>
2019-09-20 15:32:44 -0400

    Fixes for showAddNewUserPanel() - Still not complete

--- CSE442Project/src/game/MainApp.java
+++ CSE442Project/src/game/MainApp.java
@@ -25,7 +25,7 @@ public class MainApp extends Application {
                 
         //showUserProfilePanel();
         
-        //showAddNewUserPanel()
+        //showAddNewUserPanel();
     }
     
     /**
