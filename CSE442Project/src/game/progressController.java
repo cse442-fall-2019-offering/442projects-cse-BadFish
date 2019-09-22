61a818765c408d3cc09b2fc6f3dc9684bdd5a1d7
yonghuil <yonghuil@buffalo.edu>
2019-09-22 15:21:22 -0400

    still testing for event controller

--- CSE442Project/src/game/progressController.java
+++ CSE442Project/src/game/progressController.java
@@ -1,5 +1,15 @@
 package game;
 
-public class progressController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 
+public class progressController {
+	public void showGame(ActionEvent event) {
+	    @FXML
+	    private ImageView image;	
+		image.setImage(arg0);
+		
+	}
 }
