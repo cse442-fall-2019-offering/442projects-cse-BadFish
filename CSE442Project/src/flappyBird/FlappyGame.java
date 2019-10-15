b54bcc97e8048a8e24f9f46bbc18834923a1927c
Mark <mark.balazon@gmail.com>
2019-10-15 15:16:28 -0400

    added bird class and config class

--- CSE442Project/src/flappyBird/FlappyGame.java
+++ CSE442Project/src/flappyBird/FlappyGame.java
@@ -1,38 +1,55 @@
 package flappyBird;
 
+import java.awt.event.ActionEvent;
+import java.time.Duration;
+
+import org.jbox2d.collision.shapes.CircleShape;
 import org.jbox2d.collision.shapes.PolygonShape;
 import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.Body;
 import org.jbox2d.dynamics.BodyDef;
+import org.jbox2d.dynamics.BodyType;
 import org.jbox2d.dynamics.FixtureDef;
 import org.jbox2d.dynamics.World;
 
-public class FlappyGame {
-	protected World m_world;
-	
-	public World getWorld() {
-	    return m_world;
+import javafx.event.EventHandler;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class FlappyGame extends Application{
+
+    
+	public void start() {
+
+
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("FlappyGame");
+        primaryStage.setFullScreen(false);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+        Group root = new Group(); //Create a group for holding all objects on the screen
+        Scene scene = new Scene(root, Config.WIDTH, Config.HEIGHT);
+        
+      //create ball   
+        final Bird ball = new Bird(45, 90, Config.BALL_RADIUS, Color.RED);
+          
+        //Add ground to the application, this is where balls will land
+        Config.addGround(100, 10);
+         
 	}
 	
-	public void init() {
-		Vec2 gravity = new Vec2(0.0f, -10.0f);
-		World world = new World(gravity);
-		
-		m_world = world;
-		
-		Body ground = null;
-		BodyDef bd = new BodyDef();
-	    ground = getWorld().createBody(bd);
-	    
-		PolygonShape shape = new PolygonShape();
-		shape.setAsBox(0.6f, 0.125f);
-		
-		FixtureDef fd = new FixtureDef();
-		fd.shape = shape;
-	    fd.density = 20.0f;
-	    fd.friction = 0.2f;
-	    
-	    world.createBody(bd);
-	}
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+	
 	
 }
